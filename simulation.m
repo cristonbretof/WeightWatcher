7b418c026a11c43c770cc3f883c508b32640711c
cristonbretof <christophe.breton.1@ulaval.ca>
2021-03-22 11:32:05 -0400

    Renforcement des uses cases pour l'outil de CAO (UI) et idéation quant à l'implantation de la simulation dans l'application

--- simulation.m
+++ simulation.m
@@ -1,9 +1,5 @@
-function [ret, msg] = simulation(link,dimPanelData,lamePanelData,...
-                            captPanelData,ampPanelData,actionPanelData)
+function [ret, msg] = simulation(app)
 %SIMULATION Summary of this function goes here
 %   Detailed explanation goes here
-    coinTableData = link.UICoinTable.Data;
-    
-    
     
 end
